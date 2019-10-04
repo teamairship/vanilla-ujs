5986a12051e3f931815987791d1394210513b7ba
Seth <sethalexander@pm.me>
2019-10-04 17:04:30 -0500

    New directory structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,67 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     mocha: {
       all: {
         options: {
-          reporter: 'Spec'
+          reporter: "Spec"
         }
       },
       ci: {
-        options: {
-        }
+        options: {}
       },
       options: {
         run: true,
-        urls: ['http://localhost:8000/index.html'],
+        urls: ["http://localhost:8000/index.html"]
       }
     },
     concat: {
       options: {
         stripBanners: true,
         banner: "(function (window, document) {\n'use strict';\n",
-        footer: '}).call(void(0), window, document);'
+        footer: "}).call(void(0), window, document);"
       },
       dist: {
         src: [
-          'lib/assets/javascripts/vanilla-ujs/polyfills.js',
-          'lib/assets/javascripts/vanilla-ujs/liteajax.js',
-          'lib/assets/javascripts/vanilla-ujs/confirm.js',
-          'lib/assets/javascripts/vanilla-ujs/method.js',
-          'lib/assets/javascripts/vanilla-ujs/disable.js',
-          'lib/assets/javascripts/vanilla-ujs/csrf.js',
-          'lib/assets/javascripts/vanilla-ujs/form.js',
+          "lib/polyfills.js",
+          "lib/liteajax.js",
+          "lib/confirm.js",
+          "lib/method.js",
+          "lib/disable.js",
+          "lib/csrf.js",
+          "lib/form.js"
         ],
-        dest: 'lib/assets/javascripts/vanilla-ujs.js'
+        dest: "lib/vanilla-ujs.js"
       }
     },
     express: {
       test: {
         options: {
           port: 8000,
-          hostname: 'localhost',
-          server: 'test/helpers/serv.js',
-          bases: ['./lib/', './test/']
+          hostname: "localhost",
+          server: "test/helpers/serv.js",
+          bases: ["./lib/", "./test/"]
         }
       }
     },
     watch: {
       tests: {
         files: ["lib/**/*.js", "test/**/*.spec.js"],
-        tasks: ["test"],
-      },
+        tasks: ["test"]
+      }
     }
   });
 
-  grunt.registerTask('test', ['express:test', 'mocha:all']);
-  grunt.registerTask('webtest', ['express:test', 'express-keepalive']);
-  grunt.registerTask('ci', ['express:test', 'mocha:ci']);
+  grunt.registerTask("test", ["express:test", "mocha:all"]);
+  grunt.registerTask("webtest", ["express:test", "express-keepalive"]);
+  grunt.registerTask("ci", ["express:test", "mocha:ci"]);
 
-  grunt.registerTask('dist', ['concat']);
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask("dist", ["concat"]);
+  grunt.registerTask("default", ["test"]);
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-express');
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-express");
+  grunt.loadNpmTasks("grunt-mocha");
 };

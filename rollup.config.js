d7d1b934b8da8de77b5747b5224a845c41586cb3
Seth <sethalexander@pm.me>
2019-10-07 10:38:35 -0500

    Setup rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
 export default {
   input: "./src/index.js",
   output: [
     { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" }
+    { file: pkg.module, format: "es" },
+    { file: pkg.browser, format: "umd" }
   ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {})
-  ]
+  plugins: [terser()]
 };
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./confirm";
+import "./csrf";
+import "./disable";
+import "./form";
+import "./liteajax";
+import "./method";
+import "./polyfills";
